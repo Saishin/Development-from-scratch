96c248537ba13d06ed9f96775b30c734fadece64
Saishin <shibachyco@gmail.com>
2019-03-16 01:40:51 +0900

    modeify make data

--- Do_files/predict_SVM.py
+++ Do_files/predict_SVM.py
@@ -2,16 +2,9 @@ import numpy as np
 from SVM import SVM
 
 def predict_SVM():
-    N = 100
-    cls1 = []
-    cls2 = []
-
-    mean1 = [-1, 2]
-    mean2 = [1, -1]
-    cov = [[1.0,0.8], [0.8, 1.0]]
-
-    cls1.extend(np.random.multivariate_normal(mean1, cov, int(N/2)))
-    cls2.extend(np.random.multivariate_normal(mean2, cov, int(N/2)))
+    N = 2000
+    cls1 = np.random.randn(1000,2)
+    cls2 = np.random.randn(1000,2)+np.array([5,5])
 
     # データ行列Xを作成
     X = np.vstack((cls1, cls2))
